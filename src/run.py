725cbb6f8245032cd00d44e411247a4e98223cac
roleyfoley <roleyfoley@gmail.com>
2019-03-29 11:35:37 +1100

    add nested record for SQS and S3 Formatting

--- src/run.py
+++ src/run.py
@@ -5,18 +5,19 @@ from ingester.ingester import Ingester
 def lambda_handler(event, context):
     
     for record in event['Records']:
-        eventName = record['eventName']
-        if eventName.startswith('ObjectCreated:') :
+        for record_event in record['Records']:
+            eventName = record_event['eventName']
+            if eventName.startswith('ObjectCreated:'):
 
-            bucket_name = record["s3"]['bucket']['name']
-            key = record['s3']['object']['key']
+                bucket_name = record_event["s3"]['bucket']['name']
+                key = record_event['s3']['object']['key']
 
-            (sinks) = Ingester.from_environment()
-            ingester = Ingester(
-                sinks=sinks,
-                bucket_name=bucket_name,
-                key=key
-            )
+                (sinks) = Ingester.from_environment()
+                ingester = Ingester(
+                    sinks=sinks,
+                    bucket_name=bucket_name,
+                    key=key
+                )
             ingester.run()
                 
     return 'complete'
